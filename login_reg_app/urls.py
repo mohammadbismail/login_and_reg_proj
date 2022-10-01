e22f47c2eed12e44f2eb34ea762c38d394c09f5b
mohammadbisamil <m.basim.ismail@gmail.com>
2022-10-01 20:37:25 +0300

    init_submit

---
+++ login_reg_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index),
+    path("register/", views.register_user),
+    path("login/", views.check_login),
+    # path("success/", views.success_page),
+    path("success/<int:user_id>", views.success_page),
+]
---
+++ login_reg_app/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from .models import User
+import bcrypt
+
+
+def index(request):
+    context = {"users": User.objects.all()}
+    return render(request, "index.html", context)
+
+
+def register_user(request):
+
+    password = request.POST["password"]
+    pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+    User.objects.create(
+        first_name=request.POST["first_name"],
+        last_name=request.POST["last_name"],
+        email=request.POST["email"],
+        password=pw_hash,
+    )
+    return redirect("/")
+
+
+def check_login(request):
+    user = User.objects.filter(email=request.POST["email"])
+    if user:  # if True (email is found with registration database)
+        logged_user = user[0]
+        if bcrypt.checkpw(
+            request.POST["password"].encode(), logged_user.password.encode()
+        ):
+            request.session["userid"] = logged_user.id
+            print(logged_user.id)
+            return redirect(f"/success/{logged_user.id}")
+        return redirect("/")
+
+
+def success_page(request, user_id):
+    context = {
+        "user": User.objects.get(id=user_id),
+    }
+    return render(request, "success.html", context)
